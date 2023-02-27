a44d8e2eac3bf2d75aff84ed67f7ed6599f6ce12
Pavel <rut100500@yandex.ru>
2023-02-27 22:19:16 +0300

    array >=3 elemrnt with method - final task

---
+++ expir1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучшеstring[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+
+
+Console.WriteLine("Введите кол-во элементов начального массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string? [] array = new string[size];
+int n = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите значения массива: ");
+    array[i] = Console.ReadLine();
+    if(array[i]!.Length <= 3)
+    {
+        n++;
+    }
+ }
+
+if (n == 0 )
+{
+    Console.WriteLine("Строк нужной размерности не найдено. Повторите процедуру!");
+    return;
+}
+else
+{
+    Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+}
+
+string? [] arrayRes = new string[n];
+int k = 0;
+for (int j = 0; j < size; j++)
+{
+    if(array[j]!.Length <= 3)
+    {
+        arrayRes[k] = array[j];
+        k++;
+        
+    }
+}
+Console.WriteLine("Результирующий массив: [" + string.Join(", ", arrayRes) + "]");
---
+++ expir2/Program.cs
@@ -0,0 +1,75 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучшеstring[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+// Вариант итогового задания с использованием метода
+
+
+Console.WriteLine("Введите кол-во элементов начального массива: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if(!isParsedN)
+{
+    Console.WriteLine("Ошибочный ввод! Не цифра! ПереВВедите!");
+    return;
+}
+
+string[] array = CreateFirstArray(n);
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+int size = GetSizeEndArray(array);
+if (size == 0)
+{
+    Console.WriteLine($"Строк нужной размерности не найдено. Повторите процедуру!");
+    return;
+}
+else
+{
+    Console.WriteLine($"Количество трех и менее значных элементов = {size}");
+}
+
+string [] arrayRes = CreateEndArray(array, size);
+Console.WriteLine("Результирующий массив: [" + string.Join(", ", arrayRes) + "]");
+
+
+string[] CreateEndArray(string [] arrayFirst, int sizeAra)
+{
+    string [] arrayRes = new string[sizeAra];
+    int k = 0;
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        if(arrayFirst[i]!.Length <= 3)
+        {
+            arrayRes[k] = arrayFirst[i];
+            k++;
+        }
+    }
+    return arrayRes;
+}
+
+
+string [] CreateFirstArray(int m)
+{
+    string [] array = new string[m];
+    for (int i = 0; i < array.Length; i++)
+    { 
+        Console.Write("Введите значения массива: ");
+        array[i] = Console.ReadLine()!; 
+    }
+    return array;
+}
+
+int GetSizeEndArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]!.Length <= 3)
+        {
+            count++;
+        }
+        
+    }
+
+    return count;  
+}
+
